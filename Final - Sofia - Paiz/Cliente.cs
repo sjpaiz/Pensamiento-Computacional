19596b199ea9549035ea9b0417bdfa7e0af05132
sjpaiz <158302451+sjpaiz@users.noreply.github.com>
2024-05-28 19:37:40 -0600

    Add files via upload

---
+++ Final - Sofia - Paiz/Cliente.cs
@@ -0,0 +1,125 @@
+﻿namespace Final___Sofia___Paiz;
+
+public class Cliente
+{
+    public string Nombre;
+    public string Apellido;
+
+    public string OpcionElegida;
+    public string ClaseElegida;
+    
+    public string SedeElegida;
+
+
+    public void PedirDatosCliente()
+    {
+        Console.WriteLine("Nombre del cliente");
+        Nombre = Console.ReadLine();
+
+        Console.WriteLine("Apellido del cliente");
+        Apellido = Console.ReadLine();
+
+    }
+
+    public void ActividadMembresia()
+    {
+
+        Console.WriteLine("¿La membresía del cliente está activa?");
+        Console.WriteLine("1. Si");
+        Console.WriteLine("2. No");
+        OpcionElegida = Console.ReadLine();
+        do
+        {
+            switch (OpcionElegida)
+            {
+                case "1":
+                    Console.WriteLine("Si posee una membresía activa");
+                    break;
+
+                case "2":
+                    Console.WriteLine("No posee membresía activa");
+                    break;
+
+                default:
+                    Console.WriteLine("La opción ingresada no es válida");
+                    OpcionElegida = "3";
+                    break;
+            }
+        } while (OpcionElegida == "3");
+
+    }
+
+    public void pedirDatosClases()
+    {
+        Console.WriteLine("¿Qué clase quiere recibir el cliente?");
+        Console.WriteLine("1. Crossfit");
+        Console.WriteLine("2. Yoga");
+        Console.WriteLine("3. Zumba");
+        Console.WriteLine("4. Circuitos");
+        Console.WriteLine("5. Spinning");
+
+        ClaseElegida = Console.ReadLine();
+
+        do
+        {
+            switch (ClaseElegida)
+            {
+                case "1":
+                    Console.WriteLine("Participará en la clase de crossfit");
+                    break;
+                case "2":
+                    Console.WriteLine("Participará en la clase de yoga");
+                    break;
+                case "3":
+                    Console.WriteLine("Participará en la clase de zumba");
+                    break;
+                case "4":
+                    Console.WriteLine("Participará en la clase de circuitos");
+                    break;
+                case "5":
+                    Console.WriteLine("Participará en la clase de spinning");
+                    break;
+                default:
+                    ClaseElegida = "6";
+                    Console.WriteLine("La opción ingresada no es válida");
+                    break;
+            }
+        } while (ClaseElegida == "6");
+    }
+
+    public void PedirDatosSede ()
+    {
+        Console.WriteLine("Sedes disponibles");
+        Console.WriteLine("1. Zona 1") ;
+        Console.WriteLine("2. Zona 2");
+        Console.WriteLine("3. Zona 10");
+
+        SedeElegida= Console.ReadLine();
+
+        do 
+        {
+            switch (SedeElegida)
+            {
+                case "1":
+                Console.WriteLine("Irá a la sede en zona 1");
+                break;
+
+                case "2":
+                Console.WriteLine("Irá a la sede en zona 2");
+                break;
+
+                case "3":
+                Console.WriteLine("Irá a la sede en zona 10");
+                break;
+                
+                default:
+                SedeElegida = "4";
+                Console.WriteLine("Ingrese una opción válida");
+                break;
+            }
+        } while (SedeElegida == "4");
+    }
+
+}
+
+
---
+++ Final - Sofia - Paiz/Program.cs
@@ -0,0 +1,106 @@
+﻿using Final___Sofia___Paiz;
+
+Cliente[] Clientes = new Cliente[2];
+
+
+Console.WriteLine("Ingresar los datos de 2 clientes");
+for (int i = 0; i < 2; i++)
+{
+    Cliente objClientes = new Cliente();
+    objClientes.PedirDatosCliente();
+    objClientes.ActividadMembresia();
+    objClientes.pedirDatosClases();
+    objClientes.PedirDatosSede();
+    Clientes[i] = objClientes;
+}
+
+string opcion = "";
+do
+{
+    Console.WriteLine("-----------------------------------------------");
+    Console.WriteLine("¿Qué quiere hacer dentro del sistema?");
+    Console.WriteLine("-----------------------------------------------");
+    Console.WriteLine("Mostrar datos");
+    Console.WriteLine("a.Imprimir lista de clientes con membresía activa");
+    Console.WriteLine("b. Consultar catálogo de clases");
+    Console.WriteLine("c. Imprimir listado de instalaciones disponibles");
+    Console.WriteLine("d. Salir");
+    opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "a":
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("LISTA DE CLIENTES CON MEMBRESÍA ACTIVA");
+            Console.WriteLine("-------------------------------------------------------");
+            for (int i = 0; i < Clientes.Length; i++)
+            {
+                if (Clientes[i] != null)
+                {
+                    if (Clientes[i].OpcionElegida == "1")
+                    {
+                        Console.WriteLine($"{Clientes[i].Nombre}{Clientes[i].Apellido} Posee membresía activa");
+                    }
+                }
+            }
+            break;
+
+        case "b":
+            for (int i = 0; i < Clientes.Length; i++)
+            {
+                if (Clientes[i] != null)
+                {
+                    if (Clientes[i].ClaseElegida == "1")
+                    {
+                        Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} hará crossfit");
+                    }
+                    if (Clientes[i].ClaseElegida == "2")
+                    {
+                        Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} hará yoga");
+                    }
+                    if (Clientes[i].ClaseElegida == "3")
+                    {
+                        Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} hará zumba");
+                    }
+                    if (Clientes[i].ClaseElegida == "4")
+                    {
+                        Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} hará circuitos");
+                    }
+                    if (Clientes[i].ClaseElegida == "5")
+                    {
+                        Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido}hará spinning");
+                    }
+                }
+            }
+            break;
+
+        case "c":
+            for (int i = 0; i < Clientes.Length; i++)
+            {
+                if (Clientes[i].SedeElegida == "1")
+                {
+                    Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} irá a Zona 1 / Disponible");
+                }
+                if (Clientes[i].SedeElegida == "2")
+                {
+                    Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} irá a Zona 2 / Disponible");
+                }
+                if (Clientes[i].SedeElegida == "3")
+                {
+                    Console.WriteLine($"{Clientes[i].Nombre} {Clientes[i].Apellido} irá a Zona 10 / Disponible");
+                }
+            }
+            break;
+        case "d":
+            Console.WriteLine("Saliendo...");
+            break;
+
+        default:
+            Console.WriteLine("Ingrese una opción válida");
+            break;
+    }
+} while (opcion != "d");
+
+
+
